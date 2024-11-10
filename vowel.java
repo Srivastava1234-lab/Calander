e51c77031f004093f06cc25a39429810baabd93b
Akshat Srivastava <akshat.sanya@gmail.com>
2024-11-10 05:22:41 +0100

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,14 @@
+int vowels = 0;
+int consonants = 0;
+String str = Terminal.readString("Quote");
+for (int k = 0; k < str.length(); k++) {
+    char c = str.charAt(k);
+    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+        vowels++;
+    }
+    else {
+        consonants++;
+    }
+}
+System.out.println("Vowel count is " + vowels);
+System.out.println("Consonant count is: " + consonants);
